c551f8b24c2a3204659e91c51e0f68052cb093b9
tufaankose <you@example.com>
2022-06-03 22:14:12 +0300

    Initial commit

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       gameScore(); 
+    }
+
+    private void gameScore()
+    {
+        scoreText.text = score.ToString();
+    }
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
